fixed test_fcoll tests to work with units

hmf/fitting_functions.py
@@ -99,7 +99,7 @@ class FittingFunction(object):
         self.params.update(model_parameters)
 
         # Save instance variables
-        self.M = M
+        self.M = M.value
         self.nu2 = nu2
         self.nu = np.sqrt(nu2)
         self.z = z

hmf/hmf.py
@@ -457,7 +457,6 @@ class MassFunction(Transfer):
         # Get required local variables
         size = len(dndm)
         m = self.M
-
         # If the highest mass is very low, we try calculating it to higher masses
         # The dlog10m is NOT CHANGED, so the input needs to be finely spaced.
         # If the top value of dndm is NaN, don't try calculating higher masses.
@@ -468,19 +467,20 @@ class MassFunction(Transfer):
             else:
                 new_mf = copy.deepcopy(self)
                 new_mf.update(Mmin=np.log10(self.M[-1].value) + self.dlog10m, Mmax=18)
-                dndm = np.concatenate((dndm, new_mf.dndm)) * dndm.unit
-                m = np.concatenate((m, new_mf.M)) * m.unit
+                dndm = np.concatenate((dndm.value, new_mf.dndm.value))
+                dndm *= new_mf.dndm.unit
 
-        print "m unit in _gtm: ", m.unit
-        print "dndm unit in _gtm: ", dndm.unit
+                m = np.concatenate((m.value, new_mf.M.value))
+                m *= new_mf.M.unit
+
         ngtm = hmf_integral_gtm(m, dndm, mass_density)
 
         # We need to set ngtm back in the original length vector with nans where they were originally
         if len(ngtm) < len(m):  # Will happen if some dndlnm are NaN
-            ngtm_temp = np.zeros_like(dndm)
+            ngtm_temp = np.zeros(len(dndm))
             ngtm_temp[:] = np.nan
-            ngtm_temp[np.logical_not(np.isnan(dndm))] = ngtm
-            ngtm = ngtm_temp
+            ngtm_temp[np.logical_not(np.isnan(dndm))] = ngtm.value
+            ngtm = ngtm_temp * ngtm.unit
 
         # Since ngtm may have been extended, we cut it back
         return ngtm[:size]

hmf/integrate_hmf.py
@@ -8,19 +8,31 @@ def hmf_integral_gtm(M, dndm, mass_density=False):
     
     Parameters
     ----------
-    M : array_like
-        Array of masses
+    M : array_like `astropy.units.Quantity` with mass units.
+        Array of masses.
         
-    dndm : array_like
+    dndm : array_like `astropy.units.Quantity` with inverse volume per mass units.
         Array of dn/dm (corresponding to M)
         
     mass_density : bool, `False`
         Whether to calculate mass density (or number density).
     """
+    if not hasattr(M, "unit"):
+        raise TypeError("M must be of quantity type")
+    if not hasattr(dndm, "unit"):
+        raise TypeError("dndm must be of quantity type")
+
     # Eliminate NaN's
     m = M[np.logical_not(np.isnan(dndm))]
     dndm = dndm[np.logical_not(np.isnan(dndm))]
     dndlnm = m * dndm
+
+    final_units = dndlnm.unit
+    m_unit = m.unit
+
+    m = m.value
+    dndm = dndm.value
+    dndlnm = dndlnm.value
     # Calculate the mass function (and its integral) from the highest M up to 10**18
     if m[-1] < m[0] * 10 ** 18 / m[3]:
         m_upper = np.arange(np.log(m[-1]), np.log(10 ** 18), np.log(m[1]) - np.log(m[0]))
@@ -38,6 +50,7 @@ def hmf_integral_gtm(M, dndm, mass_density=False):
     if not mass_density:
         ngtm = np.concatenate((cumtrapz(dndlnm[::-1], dx=np.log(m[1]) - np.log(m[0]))[::-1], np.zeros(1)))
     else:
+        final_units *= m_unit
         ngtm = np.concatenate((cumtrapz(m[::-1] * dndlnm[::-1], dx=np.log(m[1]) - np.log(m[0]))[::-1], np.zeros(1)))
 
-    return (ngtm + int_upper) * dndlnm.unit
+    return (ngtm + int_upper) * final_units

tests/test_fcoll.py
@@ -58,7 +58,7 @@ class TestCumulants(object):
         hmf.update(Mmin=minm, Mmax=maxm)
         anl = fcoll_Peacock(np.sqrt(hmf.nu))
         num = hmf.rho_gtm / hmf.mean_density0
-        err = np.abs((num - anl) / anl)[np.logical_and(hmf.M > 10 ** 10, hmf.M < 10 ** 15)]
+        err = np.abs((num - anl) / anl)[np.logical_and(hmf.M.value > 10 ** 10, hmf.M.value < 10 ** 15)]
         err = err[np.logical_not(np.isnan(err))]
         print np.max(err)
         assert np.max(err) < TestCumulants.tol
@@ -79,8 +79,8 @@ class TestCumulants(object):
 
     def check_mgtm(self, hmf, maxm):
         hmf.update(Mmin=0, Mmax=maxm, dlog10m=0.01)
-        print hmf.rho_gtm.unit
-        print hmf.mean_density0.unit
+        print "rhogtm: ", hmf.rho_gtm.unit
+        print "rhomean:", hmf.mean_density0.unit
         assert np.abs(hmf.rho_gtm[0] / hmf.mean_density0 - 1) < 0.1  # THIS IS PRETTY BIG!
 
 