ffe254bd4102c4353ccd6fed3b228639093fe3ce
Steven Murray <steven.jeanette.m@gmail.com>
2015-02-10 08:59:19 +0800

    test_fits running ok

--- tests/test_fits.py
+++ tests/test_fits.py
@@ -9,6 +9,7 @@ import sys
 sys.path.insert(0, LOCATION)
 from hmf import MassFunction
 from hmf import fitting_functions as ff
+import copy
 def check_close(hmf, close_hmf, fit, redshift):
     hmf.update(z=redshift, mf_fit=fit)
     close_hmf.update(z=redshift)
@@ -18,14 +19,13 @@ def check_close(hmf, close_hmf, fit, redshift):
     assert np.mean(np.abs(diff[inds] / hmf.fsigma[inds])) < 1
 
 def test_all_fits():
-    hmf = MassFunction(Mmin=10, Mmax=15, dlog10m=0.1, omegab=0.05, omegac=0.25,
-                       omegav=0.7, sigma_8=0.8, n=1, H0=70.0,
-                       lnk_min=-16, lnk_max=10, dlnk=0.01, mf_fit='ST', z=0.0)
-    close_hmf = MassFunction(Mmin=10, Mmax=15, dlog10m=0.1, omegab=0.05, omegac=0.25,
-                       omegav=0.7, sigma_8=0.8, n=1, H0=70.0,
-                       lnk_min=-16, lnk_max=10, dlnk=0.01, mf_fit='ST', z=0.0)
+    hmf = MassFunction(Mmin=10, Mmax=15, dlog10m=0.1,
+                       lnk_min=-16, lnk_max=10, dlnk=0.01,
+                       mf_fit='ST', z=0.0, sigma_8=0.8, n=1, Ob0=0.05,
+                       cosmo_params={"Om0":0.3, "H0":70.0})
+
     hmf.fsigma
-    close_hmf.fsigma
+    close_hmf = copy.deepcopy(hmf)
     ff._allfits.remove("AnguloBound")
 
     for redshift in [0.0, 2.0]:
