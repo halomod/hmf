ee75d6e49a3676bb3b8088dab07f6333b523de48
Steven Murray <steven.jeanette.m@gmail.com>
2015-03-04 16:19:22 +0800

    installing hmf-fit

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     install_requires=["numpy>=1.6.2",
                       "scipy>=0.12.0",
                       "astropy>=1.0"],
-    scripts=["scripts/hmf"],
+    scripts=["scripts/hmf", "scripts/hmf-fit"],
     author="Steven Murray",
     author_email="steven.murray@uwa.edu.au",
     description="A halo mass function calculator",
