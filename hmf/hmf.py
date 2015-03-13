badebcf69c46c2ac2dd9f396a8df0865646f5022
Steven Murray <steven.jeanette.m@gmail.com>
2015-03-13 16:42:09 +0800

    bumped version

--- hmf/hmf.py
+++ hmf/hmf.py
@@ -5,7 +5,7 @@ The module contains a single class, `MassFunction`, which wraps almost all the
 functionality of :mod:`hmf` in an easy-to-use way.
 '''
 
-version = '1.8.0'
+version = '2.0.0'
 
 ###############################################################################
 # Some Imports
