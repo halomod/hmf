9cbe0f219102fc4dd45097d3a318c092765f84b9
Steven Murray <steven.jeanette.m@gmail.com>
2015-09-09 17:23:18 +0800

    added a more basic get_model_

--- hmf/_framework.py
+++ hmf/_framework.py
@@ -7,8 +7,8 @@ from _cache import Cache
 class Model(object):
     """
     Class representing a component model.
-    
-    All component models in the framework should be subclassed from this. The 
+
+    All component models in the framework should be subclassed from this. The
     features of this class are that it contains a class variable called _defaults
     which contains the defaults for the parameters of the specific model which
     subclasses this. These are checked and updated with passed parameters by
@@ -30,34 +30,47 @@ class Model(object):
         self.params = copy.copy(self._defaults)
         self.params.update(model_params)
 
-
-def get_model(name, mod, **kwargs):
+def get_model_(name, mod):
     """
-    Returns an instance of ``name`` from the module ``mod``, with given params.
-    
+    Returns a class ``name`` from the module ``mod``.
+
     Parameters
     ----------
     name : str
         The class name of the appropriate model
-        
+
     mod : str
         The module name of the appropriate module
-        
-    \*\*kwargs : 
+    """
+    return getattr(sys.modules[mod], name)
+
+def get_model(name, mod, **kwargs):
+    """
+    Returns an instance of ``name`` from the module ``mod``, with given params.
+
+    Parameters
+    ----------
+    name : str
+        The class name of the appropriate model
+
+    mod : str
+        The module name of the appropriate module
+
+    \*\*kwargs :
         Any parameters for the instantiated model (including model parameters)
     """
-    return getattr(sys.modules[mod], name)(**kwargs)
+    return get_model_(name,mod)(**kwargs)
 
 class Framework(Cache):
     """
     Class representing a coherent framework of component models.
-    
+
     The specific subclasses of this class should be composed of methods that are
     decorated with either ``@_cache.parameter`` for things that are parameters,
-    or ``@_cache.cached_property`` for derived quantities. 
-    
+    or ``@_cache.cached_property`` for derived quantities.
+
     Other methods are permissable, but may complicate matters if a derived
-    quantity uses the non-cached_property method. Reserve these for utility 
+    quantity uses the non-cached_property method. Reserve these for utility
     methods.
     """
     def __init__(self):
