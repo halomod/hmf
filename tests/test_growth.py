07224a1f8fb6a75fd98324583575c752e46f5237
steven-murray <steven.murray@curtin.edu.au>
2016-02-16 15:32:34 +0800

    tests for growth functions

---
+++ tests/test_growth.py
@@ -0,0 +1,50 @@
+import numpy as np
+import inspect
+import os
+LOCATION = "/".join(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))).split("/")[:-1])
+import sys
+sys.path.insert(0, LOCATION)
+from hmf import growth_factor as gf
+from hmf.cosmo import Planck13
+
+
+class TestSimilarity(object):
+    """
+    Simply test similarity between standard GrowthFunction and others (at this point, just GenMFGrowth)
+    that should be similar.
+    """
+    def __init__(self):
+        self.gf = gf.GrowthFactor(Planck13)
+        self.genf = gf.GenMFGrowth(Planck13,zmax=10.0)
+
+    def test_gf(self):
+        for z in np.arange(0,8,0.5):
+            print self.gf.growth_factor(z),self.genf.growth_factor(z)
+            assert np.isclose(self.gf.growth_factor(z),self.genf.growth_factor(z),rtol=1e-2 + z/500.0)
+
+    def test_gr(self):
+        for z in np.arange(0,8,0.5):
+            self.gf.growth_rate(z),self.genf.growth_rate(z)
+            assert np.isclose(self.gf.growth_rate(z),self.genf.growth_rate(z),rtol=1e-2+ z/100.0)
+
+    def test_gfunc(self):
+        gf_func = self.gf.growth_factor_fn(0.0)
+        genf_func = self.genf.growth_factor_fn(0.0)
+
+        print gf_func(np.linspace(0,5,10)),genf_func(np.linspace(0,5,10))
+        assert np.allclose(gf_func(np.linspace(0,5,10)),genf_func(np.linspace(0,5,10)),rtol=1e-2)
+
+    def test_gr_func(self):
+        gr_func = self.gf.growth_rate_fn(0.0)
+        genf_func = self.genf.growth_rate_fn(0.0)
+
+        print gr_func(np.linspace(0,5,10)),genf_func(np.linspace(0,5,10))
+        assert np.allclose(gr_func(np.linspace(0,5,10)),genf_func(np.linspace(0,5,10)),rtol=1e-2)
+
+    def test_inverse(self):
+        gf_func = self.gf.growth_factor_fn(0.0,inverse=True)
+        genf_func = self.genf.growth_factor_fn(0.0,inverse=True)
+
+        gf = np.linspace(0.15,0.99,10)
+        print gf_func(gf),genf_func(gf)
+        assert np.allclose(gf_func(gf),genf_func(gf),rtol=1e-1)
