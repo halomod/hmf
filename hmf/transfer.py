removed some potential redundancy in normalisation, fixed "filter" ref

hmf/transfer.py
@@ -227,16 +227,18 @@ class Transfer(cosmo.Cosmology):
         """
         return self.k ** self.n * np.exp(self._unnormalised_lnT) ** 2
 
-
-    @cached_property("filter", "dlnk", "transfer","n")
+    @cached_property("dlnk", "transfer","n")
     def _unn_sig8(self):
         # Always use a TopHat for sigma_8, and always use full k-range
-        lnk = np.arange(-8, 8, self.dlnk)
-        t = self.transfer.lnt(lnk)
-        p = np.exp(lnk) ** self.n * np.exp(t) ** 2
-        filter = filters.TopHat(np.exp(lnk),p)
+        if self.lnk_min > -15 or self.lnk_max < 9:
+            lnk = np.arange(-8, 8, self.dlnk)
+            t = self.transfer.lnt(lnk)
+            p = np.exp(lnk) ** self.n * np.exp(t) ** 2
+            filt = filters.TopHat(np.exp(lnk),p)
+        else:
+            filt = filters.TopHat(self.k,self._unnormalised_power)
 
-        return filter.sigma(8.0)[0]
+        return filt.sigma(8.0)[0]
 
     @cached_property("_unn_sig8", "sigma_8")
     def _normalisation(self):