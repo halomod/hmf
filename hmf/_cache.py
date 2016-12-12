70d8db5c1774486c5da0b8f9d6cd5e90315f27fa
steven-murray <steven.murray@curtin.edu.au>
2016-12-12 13:20:13 +0800

    removed Cache class dependency

--- hmf/_cache.py
+++ hmf/_cache.py
@@ -8,11 +8,12 @@ updated.
 """
 from functools import update_wrapper
 
-class Cache(object):
-    def __init__(self):
-        self.__recalc_prop_par = {}
-        self.__recalc_par_prop = {}
-        self.__recalc = {}
+def hidden_loc(obj,name):
+    """
+    Generate the location of a hidden attribute.
+    Importantly deals with attributes beginning with an underscore.
+    """
+    return ("_" + obj.__class__.__name__ + "__"+ name).replace("___", "__")
 
 def cached_property(*parents):
     """
@@ -39,20 +40,20 @@ def cached_property(*parents):
     calculation will be re-performed.
     """
 
-    recalc = "_Cache__recalc"
-    recalc_prpa = "_Cache__recalc_prop_par"
-    recalc_papr = "_Cache__recalc_par_prop"
-
     def cache(f):
         name = f.__name__
 
-        prop_ext = '__%s' % name
-
         def _get_property(self):
-            prop = ("_" + self.__class__.__name__ + prop_ext).replace("___", "__")
+            # Location of the property to be accessed
+            prop = hidden_loc(self,name)
+
+            # Locations of indexes
+            recalc = hidden_loc(self,"recalc")
+            recalc_prpa = hidden_loc(self,"recalc_prop_par")
+            recalc_papr = hidden_loc(self,"recalc_par_prop")
 
             # If recalc is constructed, and it needs to be updated,
-            # or ifq recalc is NOT constructed, recalculate
+            # or if recalc is NOT constructed, recalculate
             if getattr(self, recalc).get(name, True):
                 value = f(self)
                 setattr(self, prop, value)
@@ -111,9 +112,14 @@ def cached_property(*parents):
         update_wrapper(_get_property, f)
 
         def _del_property(self):
+            # Locations of indexes
+            recalc = hidden_loc(self,"recalc")
+            recalc_prpa = hidden_loc(self,"recalc_prop_par")
+            recalc_papr = hidden_loc(self,"recalc_par_prop")
+
             # Delete the property AND its recalc dicts
             try:
-                prop = ("_" + self.__class__.__name__ + prop_ext).replace("___", "__")
+                prop = hidden_loc(self,name)
                 delattr(self, prop)
                 del getattr(self, recalc)[name]
                 del getattr(self, recalc_prpa)[name]
@@ -167,18 +173,19 @@ def parameter(f):
     """
 
     name = f.__name__
-    prop_ext = '__%s' % name
     par_ext = "__parameters"
-    recalc = "_Cache__recalc"
-    recalc_papr = "_Cache__recalc_par_prop"
 
     def _set_property(self, val):
-#        prop = ("_" + self.__class__.__name__ + prop_ext).replace("___", "__")
+        prop = hidden_loc(self, name)
+
         # The following does any complex setting that is written into the code
         val = f(self, val)
 
-        # The actual location of the property
-        prop = ("_" + self.__class__.__name__ + prop_ext).replace("___", "__")
+        # Locations of indexes
+        recalc = hidden_loc(self, "recalc")
+        recalc_prpa = hidden_loc(self, "recalc_prop_par")
+        recalc_papr = hidden_loc(self, "recalc_par_prop")
+        parameters = hidden_loc(self,"parameters")
 
         try:
             # If the property has already been set, we can grab its old value
@@ -193,13 +200,18 @@ def parameter(f):
             # It's not been set before, so add it to our list of parameters
             try:
                 # Only works if something has been set before
-                parlist =  getattr(self,("_" + self.__class__.__name__ + par_ext).replace("___", "__"))
+                parlist =  getattr(self,parameters)
                 parlist += [name]
-                setattr(self,("_" + self.__class__.__name__ + par_ext).replace("___", "__"),parlist)
+                setattr(self,parameters,parlist)
             except AttributeError:
                 # If nothing has ever been set, start a list
-                setattr(self, ("_" + self.__class__.__name__ + par_ext).replace("___", "__"), [name])
+                setattr(self, parameters, [name])
 
+                # Given that *at least one* parameter must be set before properties are calculated,
+                # we can define the original empty indexes here.
+                setattr(self, recalc, {})
+                setattr(self, recalc_prpa, {})
+                setattr(self, recalc_papr, {})
 
         # If either the new value is different from the old, or we never set it before
         if not obj_eq(val, old_val) or doset:
@@ -217,7 +229,7 @@ def parameter(f):
     update_wrapper(_set_property, f)
 
     def _get_property(self):
-        prop = ("_" + self.__class__.__name__ + prop_ext).replace("___", "__")
+        prop = hidden_loc(self,name)
         return getattr(self, prop)
 
     # Here we set the documentation
