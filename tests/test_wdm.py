c7b73e6a7288ec96f4c2fc673aff2b3d35f10e93
steven-murray <steven.murray@curtin.edu.au>
2017-05-19 14:31:57 +0800

    make test_wdm

---
+++ tests/test_wdm.py
@@ -0,0 +1,9 @@
+import inspect
+import os
+
+LOCATION = "/".join(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))).split("/")[:-1])
+from nose.tools import raises
+import sys
+sys.path.insert(0, LOCATION)
+from hmf.cosmo import Cosmology, WMAP7
+import numpy as np
