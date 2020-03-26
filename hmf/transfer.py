e102bc4c051ff710358ca03c4fb95c696a34b9c3
jlashner <jlashner@gmail.com>
2020-03-26 15:06:58 -0700

    Adds use_splined_growth argument

--- hmf/transfer.py
+++ hmf/transfer.py
@@ -42,7 +42,7 @@ class Transfer(cosmo.Cosmology):
     def __init__(self, sigma_8=0.8159, n=0.9667, z=0.0, lnk_min=np.log(1e-8),
                  lnk_max=np.log(2e4), dlnk=0.05, transfer_model=tm.CAMB if HAVE_PYCAMB else tm.EH,
                  transfer_params=None, takahashi=True, growth_model=gf.GrowthFactor,
-                 growth_params=None, **kwargs):
+                 growth_params=None, use_splined_growth=False, **kwargs):
 
         # Call Cosmology init
         super(Transfer, self).__init__(**kwargs)
@@ -52,6 +52,7 @@ class Transfer(cosmo.Cosmology):
         self.sigma_8 = sigma_8
         self.growth_model = growth_model
         self.growth_params = growth_params or {}
+        self.use_splined_growth = use_splined_growth
         self.lnk_min = lnk_min
         self.lnk_max = lnk_max
         self.dlnk = dlnk
@@ -269,7 +270,7 @@ class Transfer(cosmo.Cosmology):
     @cached_quantity
     def _growth_factor_fn(self):
         """
-            Function that efficiently returns the growth factor.
+        Function that efficiently returns the growth factor.
         """
         return self.growth.growth_factor_fn()
 
@@ -278,7 +279,10 @@ class Transfer(cosmo.Cosmology):
         r"""
         The growth factor
         """
-        return self._growth_factor_fn(self.z)
+        if self.use_splined_growth:
+            return self._growth_factor_fn(self.z)
+        else:
+            return self.growth.growth_factor(self.z)
 
     @cached_quantity
     def power(self):
