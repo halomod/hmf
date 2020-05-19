79c0d64f28efb5a5f3edec8c7655040c9af8ee56
Steven Murray <steven.murray@curtin.edu.au>
2020-05-19 14:27:30 -0700

    Add tests via Actions

---
+++ ci/install_deps.sh
@@ -0,0 +1,13 @@
+set -xe
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+conda info -a
+conda create --name=${ENV_NAME}  python=$PYTHON --quiet
+#conda env update -f ci/${ENV_NAME}.yaml
+source activate ${ENV_NAME}
+conda list -n ${ENV_NAME}
+# check that the python version matches the desired one; exit immediately if not
+PYVER=`python -c "from __future__ import print_function; import sys; print('{:d}.{:d}'.format(sys.version_info.major, sys.version_info.minor))"`
+if [[ $PYVER != $PYTHON ]]; then
+  exit 1;
+fi
