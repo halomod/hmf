6e45b97e5c654d72cd168bf6c8042166e37b8f64
Steven Murray <steven.murray@curtin.edu.au>
2020-05-19 10:20:11 -0700

    use setuptools

--- hmf/density_field/__init__.py
+++ hmf/density_field/__init__.py
@@ -2,3 +2,5 @@
 A subpackage dedicated to basic measures of the matter density field -- its 2-point structure, cosmological transfer
 functions, and filter functions which can be applied to it.
 """
+from .transfer import Transfer
+from .transfer_models import CAMB
